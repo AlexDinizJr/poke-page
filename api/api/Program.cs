30dc3e474f7a726f601a79917a117bf9f04ad955
AlexDinizJr <alexdinizjr13@gmail.com>
2026-02-23 15:21:12 -0300

    feat: add OpenAPI + Scalar UI for API documentation

--- api/api/Program.cs
+++ api/api/Program.cs
@@ -1,10 +1,24 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(options =>
+{
+    options.AddDocumentTransformer((document, context, cancellationToken) =>
+    {
+        document.Info = new()
+        {
+            Title = "PokePage API",
+            Version = "v1"
+        };
+
+        return Task.CompletedTask;
+    });
+});
 
 var app = builder.Build();
 
@@ -12,6 +26,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
